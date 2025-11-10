86720ff679b078790af49be79d20fd6789243bdd
CypherBit Technologies <cypherbit.technologies@gmail.com>
2025-11-10 17:49:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Cypherbit-Website/',   // EXACT repo name
+  base: '/',   // IMPORTANT for custom domain
 })
